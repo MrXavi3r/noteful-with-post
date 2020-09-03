4e2f0823ae25406f79cd69346491c1d958373fcc
Mark <SwiftTactics@gmail.com>
2020-09-03 14:33:35 -0400

    adding notes works!

--- src/AddNote.js
+++ src/AddNote.js
@@ -3,23 +3,42 @@ import ApiContext from './ApiContext'
 import config from './config'
 
 
+/*
+
+let newDate = new Date()
+        
+        console.log(newDate.toISOString()) 
+
+*/
+
+
 class AddNote extends React.Component {
-  state = {  }
+  state = { 
+    name:'',
+    folderId:'',
+    content:'',
+    modified:''
+
+
+   }
 
   static contextType = ApiContext;
 
   generateFolderList = () => {
     const folderList = this.context.folders.map(item => {
-      return <option key={item.id}>{item.name}</option>
+      return <option key={item.id} value={item.id}>{item.name}</option>
     })
     return folderList
   }
 
   handleClickAddNote = (name, description, folder) => {
+   let newDate = new Date().toISOString()
+   console.log(newDate)
     let newItem = JSON.stringify({
       name: name,
       folderId: folder,
-      content: description
+      content: description,
+      modified: newDate
     })
 
     fetch(`${config.API_ENDPOINT}/notes`, {
@@ -34,19 +53,50 @@ class AddNote extends React.Component {
     
   }
 
-  render() { 
+  getName = (name) => {
+    this.setState({
+      name:name
+    })
 
+  }
+
+  getFolder = (folder) => {
+    this.setState({
+      folderId: folder
+    })
+
+  }
+  getContent = (content) => {
+    this.setState({
+      content: content
+    })
+
+  }
+
+
+
+
+  render() { 
+    
 
     return (
 
       <form>
         <label htmlFor="add-folder">Add Folder</label>
-        <select id="add-folder">{this.generateFolderList()}</select>
+        <select id="add-folder"
+        onChange={(e) => this.getFolder(e.target.value)}
+        >{this.generateFolderList()}</select>
         <label htmlFor="note-name">Note Name</label>
-        <input id="note-name"></input>
+        <input 
+          id="note-name"
+          onChange={(e) => this.getName(e.target.value)}
+        >
+        </input>
         <label htmlFor="add-description">Add Description</label>
-        <textarea name="add-description" id="add-description" cols="30" rows="10"></textarea>
-        <button type="button" onClick={()=> this.handleClickAddNote('Food3', 'this is a food item', 'b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1')}>Add Note</button>
+        <textarea name="add-description" id="add-description" cols="30" rows="10"
+        onChange={(e) => this.getContent(e.target.value)}
+        ></textarea>
+        <button type="button" onClick={()=> this.handleClickAddNote(this.state.name, this.state.content, this.state.folderId)}>Add Note</button>
       </form>
 
       );
